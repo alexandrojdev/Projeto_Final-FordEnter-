675e87c4952207f7328884d415bad732d84f0d4a
Alexandro Jesus <alexandrosjesus10@gmail.com>
2025-10-09 13:32:12 -0300

    Realizado estilização da pg-estabelecimentos.

--- src/app/componentes/pg-estabelecimentos/pg-estabelecimentos.component.ts
+++ src/app/componentes/pg-estabelecimentos/pg-estabelecimentos.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // <-- ESSENCIAL para *ngFor e o pipe 'currency'
 import { CabecalhoDoisComponent } from '../cabecalho-dois/cabecalho-dois.component';
+import { BuscarServicosComponent } from '../buscar-servicos/buscar-servicos.component';
+import { CabecalhoUmComponent } from '../cabecalho-um/cabecalho-um.component';
+import { FooterPrincipalComponent } from '../footer-principal/footer-principal.component';
 
 // Importe o CarouselModule para que o HTML reconheça o carrossel
 import { CarouselModule } from 'ngx-bootstrap/carousel';
@@ -13,7 +16,7 @@ import { Estabelecimentos } from './estabelecimentos.model';
   standalone: true, // <-- Define que este é um componente independente
   imports: [
     CommonModule,    // <-- Permite o uso de diretivas como *ngFor, *ngIf, etc.
-    CarouselModule, CabecalhoDoisComponent,   // <-- Torna os componentes do carrossel (carousel, slide) disponíveis no template
+    CarouselModule, CabecalhoDoisComponent, FooterPrincipalComponent, BuscarServicosComponent, CabecalhoUmComponent  // <-- Torna os componentes do carrossel (carousel, slide) disponíveis no template
   ],
   templateUrl: './pg-estabelecimentos.component.html',
   styleUrl: './pg-estabelecimentos.component.css'
@@ -24,13 +27,13 @@ export class PgEstabelecimentosComponent {
   estabelecimentos: Estabelecimentos[] = [
     {
       id: 1,
-      nome: 'Beleza Natural',
-      endereco: 'Rua das Tesouras, 123, Centro',
-      imagemUrl: '/img_beleza_natural.webp',
+      nome: 'Beleza Divina',
+      endereco: 'Rua das Hortênsias, 123 - Pituba',
+      imagemUrl: '/salao_beleza1.png',
       avaliacao: 4.8,
       servicos: [
-        { nome: 'Tratamentos e Revitalização', preco: 40.00 },
-        { nome: 'bn.Volumais', preco: 35.00 },
+        { nome: 'Cortes Modernos', preco: 40.00 },
+        { nome: 'Penteado para eventos', preco: 80.00 },
         { nome: 'ULTRA CACHOS', preco: 15.00 },
         { nome: 'Texturização', preco: 15.00 }
 
@@ -38,26 +41,25 @@ export class PgEstabelecimentosComponent {
     },
     {
       id: 2,
-      nome: 'Studio de Beleza Moderno',
-      endereco: 'Avenida das Estrelas, 500, Jardins',
-      imagemUrl: 'https://images.unsplash.com/photo-1521590832167-7bcbfaa6381f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDV8fHNhbG9ufGVufDB8fHx8MTY3OTg1MjY2NQ&ixlib=rb-4.0.3&q=80&w=800',
+      nome: 'Glamour Express',
+      endereco: 'Rua Professora Magalhães Neto, 789 - Costa Azul',
+      imagemUrl: '/salao_beleza2.png',
       avaliacao: 4.9,
       servicos: [
-        { nome: 'Manicure e Pedicure', preco: 70.00 },
-        { nome: 'Design de Sobrancelha', preco: 45.00 },
-        { nome: 'Limpeza de Pele', preco: 120.00 }
+        { nome: 'Escova Express', preco: 70.00 },
+        { nome: 'Cortes rápidos', preco: 45.00 },
       ]
     },
     {
       id: 3,
-      nome: 'Navalha & Cia',
-      endereco: 'Travessa do Fio, 45, Vila Madalena',
-      imagemUrl: 'https://images.unsplash.com/photo-1621607512214-6c34903434ef?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDEyfHxiYXJiZXJzaG9wfGVufDB8fHx8MTY3OTg1MjYyOQ&ixlib=rb-4.0.3&q=80&w=800',
+      nome: 'Cabelo & Arte',
+      endereco: 'Largo de Santana, 303 - Rio Vermelho',
+      imagemUrl: '/salao_beleza3.png',
       avaliacao: 4.7,
       servicos: [
-        { nome: 'Corte + Barba', preco: 70.00 },
-        { nome: 'Coloração', preco: 90.00 },
-        { nome: 'Hidratação Capilar', preco: 50.00 }
+        { nome: 'Coloração fantasiosa', preco: 70.00 },
+        { nome: 'Maquiagem performática', preco: 90.00 },
+        { nome: 'Body painting', preco: 350.00 }
       ]
     }
   ];
